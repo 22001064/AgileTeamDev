1ac3a8e7e57b8e731bed9840e83624f79dcd3a43
EgleUrl <47123888+EgleUrl@users.noreply.github.com>
2025-02-25 16:41:51 +0000

    Improved Footer
    Deleted social media icons, written slogan

--- Frontend/src/Components/footer.jsx
+++ Frontend/src/Components/footer.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { FaFacebookF, FaHome, FaInstagram, FaLinkedinIn } from "react-icons/fa";
-import { FaPhone } from "react-icons/fa";
-import { FaEnvelope } from "react-icons/fa";
 import './footer.css';
 
 const Footer = () => {
@@ -10,60 +7,22 @@ const Footer = () => {
             {/*Grid container*/}
             <div className="container p-4">
                 {/*Grid row*/}
-                <div className="row">
+                <div className="row">                     
                     {/*Grid column*/}
                     <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
                         <div className='longlogo'>
                             <a href='/nestcafe/'><img src="./src/assets/logolong.png?raw=true" className='rounded' alt="Company logo" id='logo'/></a>
-                        </div>
-                        <h6 className='pt-4 pb-3'>
-                            Here can be your nice company Slogan
-                        </h6>
-                        {/*Section: Social media*/}
-                        <section className="mb-4">
-                            {/*Facebook*/}
-                            <a
-                                className="btn text-white btn-floating m-1"
-                                style={{backgroundColor: "#3b5998"}}
-                                href="#!"
-                                role="button"
-                                id="social"
-                                ><FaFacebookF/>
-                            </a>
-                            {/*Twitter*/}
-                            <a
-                                className="btn text-white btn-floating m-1"
-                                style={{backgroundColor: "#333333"}}
-                                href="#!"
-                                role="button"
-                                id="social"
-                            ><b>X</b></a>
-
-                            {/*Instagram*/}
-                            <a
-                                className="btn text-white btn-floating m-1"
-                                style={{backgroundColor: "#ac2bac"}}
-                                href="#!"
-                                role='button'
-                                id="social"
-                                ><FaInstagram/></a>
-
-                            {/*Linkedin*/}
-                            <a
-                                className="btn text-white btn-floating m-1"
-                                style={{backgroundColor: "#0082ca"}}
-                                href="#!"
-                                role='button'
-                                id="social"
-                                ><FaLinkedinIn/></a>
-                        </section>
-                        {/*Section: Social media*/}
+                        </div>                                     
                     </div>
-                    {/*Grid column*/}                    
                     {/*Grid column*/}
-
+                    <div className="col-lg-4 col-md-12 mb-4 mb-md-0" id="footertext">
+                        <h6 className='text-uppercase fw-bold pt-4 pb-3'>
+                            Together we can go higher!
+                        </h6>                       
+                    </div> 
                     {/*Grid column*/}
-                    <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
+                    {/*Grid column*/}
+                    <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
                         <h6 className="text-uppercase fw-bold">Site Links</h6>
                         <ul className="list-unstyled mb-0">
                             <li className='link'>
@@ -71,32 +30,10 @@ const Footer = () => {
                             </li>
                             <li className='link'>
                                 <a href="/nestcafe/login" className="text-black">Login</a>
-                            </li>
-                            <li className='link'>
-                                <a href="/nestcafe/tasks" className="text-black">Tasks</a>
-                            </li>                            
+                            </li>                                                   
                         </ul>
                     </div>
-                    {/*Grid column*/}
-
-                    {/*Grid column*/}
-                    <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
-                        <h6 className="text-uppercase fw-bold">Other Links</h6>
-                        <ul className="list-unstyled mb-0">
-                            <li className='link'>
-                                <a href="https://www.costa.co.uk/" target='_blank' className="text-black">Our partners</a>
-                            </li>                            
-                        </ul>
-                    </div>
-                    {/*Grid column*/}
-                    <div className="col-lg-3 col-md-12 mb-4 mb-md-0">          
-                        <h6 className="text-uppercase fw-bold mb-1">Contact</h6>
-                        
-                        <p><FaHome /> 53 Cowgate, Peterborough, UK</p>
-                        <p><FaEnvelope/> info@netscafe.co.uk </p>
-                        <p><FaPhone/> 01733 312546</p>
-                    </div>
-                        {/*Grid column*/}
+                    {/*Grid column*/}                   
                 </div>
                 {/*Grid row*/}
             </div>
