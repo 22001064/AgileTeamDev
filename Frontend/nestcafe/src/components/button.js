faf1cea8348fa1ebcbaae22b96e5332d39b4a33d
Sweetcreeps <135039113+Sweetcreeps@users.noreply.github.com>
2025-03-16 20:47:47 +0000

    removed images made pop ups for the support and privacy policy buttons and made the page not scrollable

--- Frontend/nestcafe/src/components/button.js
+++ Frontend/nestcafe/src/components/button.js
@@ -1,16 +1,39 @@
-import React from 'react'
-import { useState } from 'react';
+// components/button.js
+
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 
-const Buttonbrn = ({title, link}) => {
-    return (
-            <div className='brownbtn'>       
-                <Button variant='outline-dark' 
-                size='lg' href={link} 
-                type="submit" 
-                id='loginbtn'>{title} 
-                </Button> 
-            </div>                     
-    );
-}
-export default Buttonbrn
+const Buttonbrn = ({ title, onClick }) => {
+  return (
+    <div className='brownbtn'>
+      <Button
+        variant='outline-dark'
+        size='lg'
+        type="button"
+        id='loginbtn'
+        onClick={onClick}  
+      >
+        {title}
+      </Button>
+    </div>
+  );
+};
+
+export default Buttonbrn;
+
+
+// changed component from:
+
+// const Buttonbrn = ({title, link, onClick}) => {
+//     return (
+//             <div className='brownbtn'>       
+//                 <Button variant='outline-dark' 
+//                 size='lg' href={link} 
+//                 type="button" 
+//                 id='loginbtn'>{title} 
+//                 onClick={onClick}
+//                 </Button> 
+//             </div>                     
+//     );
+// }
+// export default Buttonbrn
--- Frontend/nestcafe/src/components/navigationBar.js
+++ Frontend/nestcafe/src/components/navigationBar.js
@@ -1,25 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 const NavigationBar = () => {
-    return (
-        <Navbar expand="lg" sticky="top" className='nav-area'>
-            <Container>
-                <Navbar.Brand href="/"><img src="../images/logo.png" className='rounded' alt="Company logo" id="logo"/></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto justify-content-end flex-grow-1 pe-4" id='links'>
-                            <Nav.Link className='link' href="/">Home</Nav.Link>
-                            <Nav.Link className='link' href="/nestcafe/login">Login</Nav.Link>
-                            <img src="/images/customer-service-support.svg" alt="Support icon"  id='supportlogo'/>
-                            <Nav.Link className='link' href="#">Support</Nav.Link>                                                         
-                        </Nav>
-                    </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
-}  
+  // State to control the Support modal
+  const [showSupportModal, setShowSupportModal] = useState(false);
+
+  // Handlers to open/close the modal
+  const handleSupportShow = () => setShowSupportModal(true);
+  const handleSupportClose = () => setShowSupportModal(false);
+
+  return (
+    <>
+      <Navbar expand="lg" sticky="top" className='nav-area'>
+        <Container>
+          <Navbar.Brand href="/">
+            <img 
+              src="../images/logo.png"
+              className='rounded'
+              alt="Company logo"
+              id="logo"
+            />
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto justify-content-end flex-grow-1 pe-4" id='links'>
+              
+              <img
+                src="/images/customer-service-support.svg"
+                alt="Support icon"
+                id='supportlogo'
+              />
+              {/* Use onClick to open the modal */}
+              <Nav.Link className='link' onClick={handleSupportShow}>
+                Support
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* Support Modal */}
+      <Modal show={showSupportModal} onHide={handleSupportClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Support</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            For assistance, please contact us at: 
+            <strong> support@fakeemail.com</strong>
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleSupportClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
 export default NavigationBar;
